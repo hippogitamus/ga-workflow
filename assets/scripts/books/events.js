3ded1a0e200f3745889f89f2982f2ee00238c60b
caleybennett <cbennet7@conncoll.edu>
2020-01-03 16:02:12 -0500

    Connected ui + handlebars to button
    Connected all the files in the events.js and got rid of some typos in UI

--- assets/scripts/books/events.js
+++ assets/scripts/books/events.js
@@ -1,12 +1,12 @@
 const api = require('./api.js')
-// const ui = require('./ui.js')
+const ui = require('./ui.js')
 
 const onGetBooks = (event) => {
   event.preventDefault()
   console.log('button works!')
   api.getBooks()
-    .then(console.log('api works yayayaw'))
-    .catch(console.error)
+    .then(ui.getBooksSuccess)
+    .catch(ui.getBooksFailure)
 }
 
 const addHandlers = event => {
